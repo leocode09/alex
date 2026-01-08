3ce6cf9603de17f8780132804b1b78d3b875d0fd
leocode09 <leocode09@gmail.com>
2026-01-08 18:19:36 +0200

    feat: Expand PIN service with additional keys for employees, stores, reports, and settings management

--- lib/services/pin_service.dart
+++ lib/services/pin_service.dart
@@ -3,12 +3,69 @@ import 'package:shared_preferences/shared_preferences.dart';
 class PinService {
   static const String _pinKey = 'user_pin';
   static const String _pinSetKey = 'pin_is_set';
+  
+  // Auth & General
   static const String _pinOnLoginKey = 'pin_on_login';
+  static const String _pinOnSettingsKey = 'pin_on_settings';
+  static const String _pinOnDashboardKey = 'pin_on_dashboard';
+  
+  // Products
   static const String _pinOnAddProductKey = 'pin_on_add_product';
   static const String _pinOnEditProductKey = 'pin_on_edit_product';
   static const String _pinOnDeleteProductKey = 'pin_on_delete_product';
-  static const String _pinOnSettingsKey = 'pin_on_settings';
+  static const String _pinOnViewProductDetailsKey = 'pin_on_view_product_details';
+  static const String _pinOnScanBarcodeKey = 'pin_on_scan_barcode';
+  static const String _pinOnAdjustStockKey = 'pin_on_adjust_stock';
+  
+  // Sales
+  static const String _pinOnCreateSaleKey = 'pin_on_create_sale';
+  static const String _pinOnViewSalesHistoryKey = 'pin_on_view_sales_history';
+  static const String _pinOnEditReceiptKey = 'pin_on_edit_receipt';
+  static const String _pinOnDeleteReceiptKey = 'pin_on_delete_receipt';
+  static const String _pinOnApplyDiscountKey = 'pin_on_apply_discount';
+  static const String _pinOnIssueRefundKey = 'pin_on_issue_refund';
+  
+  // Categories
+  static const String _pinOnAddCategoryKey = 'pin_on_add_category';
+  static const String _pinOnEditCategoryKey = 'pin_on_edit_category';
+  static const String _pinOnDeleteCategoryKey = 'pin_on_delete_category';
+  static const String _pinOnViewCategoriesKey = 'pin_on_view_categories';
+  
+  // Customers
+  static const String _pinOnViewCustomersKey = 'pin_on_view_customers';
+  static const String _pinOnAddCustomerKey = 'pin_on_add_customer';
+  static const String _pinOnEditCustomerKey = 'pin_on_edit_customer';
+  static const String _pinOnDeleteCustomerKey = 'pin_on_delete_customer';
+  
+  // Employees
+  static const String _pinOnViewEmployeesKey = 'pin_on_view_employees';
+  static const String _pinOnAddEmployeeKey = 'pin_on_add_employee';
+  static const String _pinOnEditEmployeeKey = 'pin_on_edit_employee';
+  static const String _pinOnDeleteEmployeeKey = 'pin_on_delete_employee';
+  
+  // Stores
+  static const String _pinOnViewStoresKey = 'pin_on_view_stores';
+  static const String _pinOnAddStoreKey = 'pin_on_add_store';
+  static const String _pinOnEditStoreKey = 'pin_on_edit_store';
+  static const String _pinOnDeleteStoreKey = 'pin_on_delete_store';
+  
+  // Reports & Analytics
   static const String _pinOnReportsKey = 'pin_on_reports';
+  static const String _pinOnViewFinancialReportsKey = 'pin_on_view_financial_reports';
+  static const String _pinOnViewInventoryReportsKey = 'pin_on_view_inventory_reports';
+  static const String _pinOnExportReportsKey = 'pin_on_export_reports';
+  
+  // System & Data Management
+  static const String _pinOnHardwareSetupKey = 'pin_on_hardware_setup';
+  static const String _pinOnDataSyncKey = 'pin_on_data_sync';
+  static const String _pinOnClearAllDataKey = 'pin_on_clear_all_data';
+  static const String _pinOnManagePromotionsKey = 'pin_on_manage_promotions';
+  static const String _pinOnViewNotificationsKey = 'pin_on_view_notifications';
+  
+  // Settings Subsections
+  static const String _pinOnTaxSettingsKey = 'pin_on_tax_settings';
+  static const String _pinOnReceiptSettingsKey = 'pin_on_receipt_settings';
+  static const String _pinOnChangePinKey = 'pin_on_change_pin';
 
   Future<bool> isPinSet() async {
     final prefs = await SharedPreferences.getInstance();
