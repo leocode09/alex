cd5e4feae660185f91555991a6806d3e99bb08b2
leocode09 <leocode09@gmail.com>
2026-01-08 23:18:48 +0200

    feat: Refactor PIN preferences storage to use a single JSON object for improved manageability

--- lib/services/pin_service.dart
+++ lib/services/pin_service.dart
@@ -1,71 +1,10 @@
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PinService {
   static const String _pinKey = 'user_pin';
   static const String _pinSetKey = 'pin_is_set';
-  
-  // Auth & General
-  static const String _pinOnLoginKey = 'pin_on_login';
-  static const String _pinOnSettingsKey = 'pin_on_settings';
-  static const String _pinOnDashboardKey = 'pin_on_dashboard';
-  
-  // Products
-  static const String _pinOnAddProductKey = 'pin_on_add_product';
-  static const String _pinOnEditProductKey = 'pin_on_edit_product';
-  static const String _pinOnDeleteProductKey = 'pin_on_delete_product';
-  static const String _pinOnViewProductDetailsKey = 'pin_on_view_product_details';
-  static const String _pinOnScanBarcodeKey = 'pin_on_scan_barcode';
-  static const String _pinOnAdjustStockKey = 'pin_on_adjust_stock';
-  
-  // Sales
-  static const String _pinOnCreateSaleKey = 'pin_on_create_sale';
-  static const String _pinOnViewSalesHistoryKey = 'pin_on_view_sales_history';
-  static const String _pinOnEditReceiptKey = 'pin_on_edit_receipt';
-  static const String _pinOnDeleteReceiptKey = 'pin_on_delete_receipt';
-  static const String _pinOnApplyDiscountKey = 'pin_on_apply_discount';
-  static const String _pinOnIssueRefundKey = 'pin_on_issue_refund';
-  
-  // Categories
-  static const String _pinOnAddCategoryKey = 'pin_on_add_category';
-  static const String _pinOnEditCategoryKey = 'pin_on_edit_category';
-  static const String _pinOnDeleteCategoryKey = 'pin_on_delete_category';
-  static const String _pinOnViewCategoriesKey = 'pin_on_view_categories';
-  
-  // Customers
-  static const String _pinOnViewCustomersKey = 'pin_on_view_customers';
-  static const String _pinOnAddCustomerKey = 'pin_on_add_customer';
-  static const String _pinOnEditCustomerKey = 'pin_on_edit_customer';
-  static const String _pinOnDeleteCustomerKey = 'pin_on_delete_customer';
-  
-  // Employees
-  static const String _pinOnViewEmployeesKey = 'pin_on_view_employees';
-  static const String _pinOnAddEmployeeKey = 'pin_on_add_employee';
-  static const String _pinOnEditEmployeeKey = 'pin_on_edit_employee';
-  static const String _pinOnDeleteEmployeeKey = 'pin_on_delete_employee';
-  
-  // Stores
-  static const String _pinOnViewStoresKey = 'pin_on_view_stores';
-  static const String _pinOnAddStoreKey = 'pin_on_add_store';
-  static const String _pinOnEditStoreKey = 'pin_on_edit_store';
-  static const String _pinOnDeleteStoreKey = 'pin_on_delete_store';
-  
-  // Reports & Analytics
-  static const String _pinOnReportsKey = 'pin_on_reports';
-  static const String _pinOnViewFinancialReportsKey = 'pin_on_view_financial_reports';
-  static const String _pinOnViewInventoryReportsKey = 'pin_on_view_inventory_reports';
-  static const String _pinOnExportReportsKey = 'pin_on_export_reports';
-  
-  // System & Data Management
-  static const String _pinOnHardwareSetupKey = 'pin_on_hardware_setup';
-  static const String _pinOnDataSyncKey = 'pin_on_data_sync';
-  static const String _pinOnClearAllDataKey = 'pin_on_clear_all_data';
-  static const String _pinOnManagePromotionsKey = 'pin_on_manage_promotions';
-  static const String _pinOnViewNotificationsKey = 'pin_on_view_notifications';
-  
-  // Settings Subsections
-  static const String _pinOnTaxSettingsKey = 'pin_on_tax_settings';
-  static const String _pinOnReceiptSettingsKey = 'pin_on_receipt_settings';
-  static const String _pinOnChangePinKey = 'pin_on_change_pin';
+  static const String _pinPreferencesKey = 'pin_preferences';
 
   Future<bool> isPinSet() async {
     final prefs = await SharedPreferences.getInstance();
@@ -140,68 +79,54 @@ class PinService {
     await prefs.setString(_pinKey, pin);
     await prefs.setBool(_pinSetKey, true);
     
-    // Auth & General
-    await prefs.setBool(_pinOnLoginKey, requireOnLogin);
-    await prefs.setBool(_pinOnSettingsKey, requireOnSettings);
-    await prefs.setBool(_pinOnDashboardKey, requireOnDashboard);
+    // Store all preferences in a single JSON key
+    final preferences = {
+      'login': requireOnLogin,
+      'settings': requireOnSettings,
+      'dashboard': requireOnDashboard,
+      'addProduct': requireOnAddProduct,
+      'editProduct': requireOnEditProduct,
+      'deleteProduct': requireOnDeleteProduct,
+      'viewProductDetails': requireOnViewProductDetails,
+      'scanBarcode': requireOnScanBarcode,
+      'adjustStock': requireOnAdjustStock,
+      'createSale': requireOnCreateSale,
+      'viewSalesHistory': requireOnViewSalesHistory,
+      'editReceipt': requireOnEditReceipt,
+      'deleteReceipt': requireOnDeleteReceipt,
+      'applyDiscount': requireOnApplyDiscount,
+      'issueRefund': requireOnIssueRefund,
+      'addCategory': requireOnAddCategory,
+      'editCategory': requireOnEditCategory,
+      'deleteCategory': requireOnDeleteCategory,
+      'viewCategories': requireOnViewCategories,
+      'viewCustomers': requireOnViewCustomers,
+      'addCustomer': requireOnAddCustomer,
+      'editCustomer': requireOnEditCustomer,
+      'deleteCustomer': requireOnDeleteCustomer,
+      'viewEmployees': requireOnViewEmployees,
+      'addEmployee': requireOnAddEmployee,
+      'editEmployee': requireOnEditEmployee,
+      'deleteEmployee': requireOnDeleteEmployee,
+      'viewStores': requireOnViewStores,
+      'addStore': requireOnAddStore,
+      'editStore': requireOnEditStore,
+      'deleteStore': requireOnDeleteStore,
+      'reports': requireOnReports,
+      'viewFinancialReports': requireOnViewFinancialReports,
+      'viewInventoryReports': requireOnViewInventoryReports,
+      'exportReports': requireOnExportReports,
+      'hardwareSetup': requireOnHardwareSetup,
+      'dataSync': requireOnDataSync,
+      'clearAllData': requireOnClearAllData,
+      'managePromotions': requireOnManagePromotions,
+      'viewNotifications': requireOnViewNotifications,
+      'taxSettings': requireOnTaxSettings,
+      'receiptSettings': requireOnReceiptSettings,
+      'changePin': requireOnChangePin,
+    };
     
-    // Products
-    await prefs.setBool(_pinOnAddProductKey, requireOnAddProduct);
-    await prefs.setBool(_pinOnEditProductKey, requireOnEditProduct);
-    await prefs.setBool(_pinOnDeleteProductKey, requireOnDeleteProduct);
-    await prefs.setBool(_pinOnViewProductDetailsKey, requireOnViewProductDetails);
-    await prefs.setBool(_pinOnScanBarcodeKey, requireOnScanBarcode);
-    await prefs.setBool(_pinOnAdjustStockKey, requireOnAdjustStock);
-    
-    // Sales
-    await prefs.setBool(_pinOnCreateSaleKey, requireOnCreateSale);
-    await prefs.setBool(_pinOnViewSalesHistoryKey, requireOnViewSalesHistory);
-    await prefs.setBool(_pinOnEditReceiptKey, requireOnEditReceipt);
-    await prefs.setBool(_pinOnDeleteReceiptKey, requireOnDeleteReceipt);
-    await prefs.setBool(_pinOnApplyDiscountKey, requireOnApplyDiscount);
-    await prefs.setBool(_pinOnIssueRefundKey, requireOnIssueRefund);
-    
-    // Categories
-    await prefs.setBool(_pinOnAddCategoryKey, requireOnAddCategory);
-    await prefs.setBool(_pinOnEditCategoryKey, requireOnEditCategory);
-    await prefs.setBool(_pinOnDeleteCategoryKey, requireOnDeleteCategory);
-    await prefs.setBool(_pinOnViewCategoriesKey, requireOnViewCategories);
-    
-    // Customers
-    await prefs.setBool(_pinOnViewCustomersKey, requireOnViewCustomers);
-    await prefs.setBool(_pinOnAddCustomerKey, requireOnAddCustomer);
-    await prefs.setBool(_pinOnEditCustomerKey, requireOnEditCustomer);
-    await prefs.setBool(_pinOnDeleteCustomerKey, requireOnDeleteCustomer);
-    
-    // Employees
-    await prefs.setBool(_pinOnViewEmployeesKey, requireOnViewEmployees);
-    await prefs.setBool(_pinOnAddEmployeeKey, requireOnAddEmployee);
-    await prefs.setBool(_pinOnEditEmployeeKey, requireOnEditEmployee);
-    await prefs.setBool(_pinOnDeleteEmployeeKey, requireOnDeleteEmployee);
-    
-    // Stores
-    await prefs.setBool(_pinOnViewStoresKey, requireOnViewStores);
-    await prefs.setBool(_pinOnAddStoreKey, requireOnAddStore);
-    await prefs.setBool(_pinOnEditStoreKey, requireOnEditStore);
-    await prefs.setBool(_pinOnDeleteStoreKey, requireOnDeleteStore);
-    
-    // Reports & Analytics
-    await prefs.setBool(_pinOnReportsKey, requireOnReports);
-    await prefs.setBool(_pinOnViewFinancialReportsKey, requireOnViewFinancialReports);
-    await prefs.setBool(_pinOnViewInventoryReportsKey, requireOnViewInventoryReports);
-    await prefs.setBool(_pinOnExportReportsKey, requireOnExportReports);
-    
-    // System & Data Management
-    await prefs.setBool(_pinOnHardwareSetupKey, requireOnHardwareSetup);
-    await prefs.setBool(_pinOnDataSyncKey, requireOnDataSync);
-    await prefs.setBool(_pinOnClearAllDataKey, requireOnClearAllData);
-    await prefs.setBool(_pinOnManagePromotionsKey, requireOnManagePromotions);
-    await prefs.setBool(_pinOnViewNotificationsKey, requireOnViewNotifications);
-    
-    // Settings Subsections
-    await prefs.setBool(_pinOnTaxSettingsKey, requireOnTaxSettings);
-    await prefs.setBool(_pinOnReceiptSettingsKey, requireOnReceiptSettings);
-    await prefs.setBool(_pinOnChangePinKey, requireOnChangePin);
+    await prefs.setString(_pinPreferencesKey, jsonEncode(preferences));
   }
 
   Future<bool> verifyPin(String pin) async {
